199ea2cb11e641ea4b47e99daa7bad6898a70898
Grace Liu <graceshiliu@gmail.com>
2024-09-30 13:24:46 -0400

    Add appbar header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Appbar from './Appbar.js';
 
 function App() {
   return (
     <div className="App">
+      <Appbar/>
       <header className="App-header">
         <p>
           Welcome to Pantry Wizard!
